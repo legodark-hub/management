42a1efeedb4f6c53f8415c8144cb91b16e9654d3
aidarminyazev <legodark@mail.ru>
2024-12-03 00:36:26 +0500

    users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from users.models import CustomUser
+from users.serializers import CustomUserSerializer
 
 # Create your views here.
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = []
