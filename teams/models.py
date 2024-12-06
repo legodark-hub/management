52a72aff2b4a1471e7831088fef12547dffd7e32
aidarminyazev <legodark@mail.ru>
2024-12-06 23:14:44 +0500

    added teams

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 # Create your models here.
@@ -6,6 +7,7 @@ class Team(models.Model):
     name = models.CharField(max_length=50, unique=True)
     admin = models.OneToOneField('users.CustomUser', on_delete=models.CASCADE, related_name='admin_team')
     created_at = models.DateTimeField(auto_now_add=True)
+    invitation_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     
     def __str__(self):
         return self.name
