f1384852d62c35a83395eaf93f0d3f765728c502
aidarminyazev <legodark@mail.ru>
2024-12-14 22:33:27 +0500

    added event types and date filter

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
-from tasks.models import Task
 from users.models import CustomUser
 
 # Create your models here.
 class SchedulerEvent(models.Model):
-    event_type = models.CharField(max_length=50, choices=[('task', 'Task')])
-    event_id = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='scheduler_events')
+    EVENT_TYPES = [
+        ('task', 'Task'),
+        ('meeting', 'Meeting'),
+    ]
+    
+    event_type = models.CharField(max_length=50, choices=EVENT_TYPES)
+    event_id = models.PositiveIntegerField()
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     event_date = models.DateTimeField()
     
     def __str__(self):
-        return f'Event for task {self.event_id.title} on {self.event_date}'
+        return f'{self.get_event_type_display()} on {self.event_date}'
