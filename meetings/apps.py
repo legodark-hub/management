adee1c37207a3bb661d291f8502671d188f4f5b4
Aidar Minyazev <54939061+legodark-hub@users.noreply.github.com>
2025-04-28 14:21:46 +0500

    Revert "added signals to work with corresponding events"
    This reverts commit 75b697c4dfbeca6b1022a42effdcdbb8f1fe4029.

--- meetings/apps.py
+++ meetings/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class MeetingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'meetings'
-    
-    def ready(self):
-        import meetings.signals
--- tasks/apps.py
+++ tasks/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
-    
-    def ready(self):
-        import tasks.signals
