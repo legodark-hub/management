0696a170073ccdf89d29e6e2a66710c211ba6456
Aidar Minyazev <54939061+legodark-hub@users.noreply.github.com>
2025-04-28 14:21:46 +0500

    Revert "added teams"
    This reverts commit 52a72aff2b4a1471e7831088fef12547dffd7e32.

--- teams/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from teams.views import TeamViewSet
-
-router = DefaultRouter()
-router.register(r"teams", TeamViewSet, basename="team")
-
-urlpatterns = router.urls
