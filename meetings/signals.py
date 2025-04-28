00bc73e708819ae5cc62cd0026cab1e3abd7ea4c
Aidar Minyazev <54939061+legodark-hub@users.noreply.github.com>
2025-04-28 14:21:46 +0500

    Revert "fix"
    This reverts commit fe560524289284012071e1554d6b9ebe1dd6d23c.

--- meetings/signals.py
+++ meetings/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete, m2m_changed
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from scheduler.models import SchedulerEvent
@@ -8,25 +8,19 @@ from .models import Meeting
 @receiver(post_save, sender=Meeting)
 def create_scheduler_event_for_meeting(sender, instance, created, **kwargs):
     if created:
-        SchedulerEvent.objects.create(
-            event_type="meeting",
-            event_id=instance.id,
-            user=instance.created_by,
-            event_date=instance.scheduled_at,
-        )
-
-
-@receiver(m2m_changed, sender=Meeting.participants.through)
-def create_calendar_events_for_participants(sender, instance, action, pk_set, **kwargs):
-    if action == "post_add":
-        for participant_id in pk_set:
-            participant = instance.participants.model.objects.get(id=participant_id)
+        for participant in instance.participants.all():
             SchedulerEvent.objects.create(
                 event_type="meeting",
                 event_id=instance.id,
                 user=participant,
                 event_date=instance.scheduled_at,
             )
+        SchedulerEvent.objects.create(
+            event_type="meeting",
+            event_id=instance.id,
+            user=instance.created_by,
+            event_date=instance.scheduled_at,
+        )
 
 
 @receiver(post_save, sender=Meeting)
@@ -39,4 +33,6 @@ def update_scheduler_event_for_meeting(sender, instance, created, **kwargs):
 
 @receiver(post_delete, sender=Meeting)
 def delete_scheduler_event_for_meeting(sender, instance, **kwargs):
-    SchedulerEvent.objects.filter(event_type="meeting", event_id=instance.id).delete()
+        SchedulerEvent.objects.filter(
+            event_type="meeting", event_id=instance.id
+        ).delete()
